9f6288aeb9bc58da9439bd08939348c762f8d627
@DoktorTa <22112000dd@gmail.com>
2024-09-25 18:25:04 +0500

    111

--- src/main/kotlin/com/example/features/auth/jwt/JwtConfig .kt
+++ src/main/kotlin/com/example/features/auth/jwt/JwtConfig .kt
@@ -6,15 +6,17 @@ import java.util.*
 
 object JwtConfig {
 
-    val secret = generateSecretKey()
+    val secret = generateSecretKey()  // ENV for secret
     val algorithm = Algorithm.HMAC256(secret)
 
+    private const val DAY: Int = 24 * 60 * 60 * 1000
+    private const val WEEK: Int = 7 * 24 * 60 * 60 * 1000  // TODO: Точно ли не лонг?
 
     fun generateAccessToken(userId: String): String {
         return JWT.create()
-            .withIssuer("your-issuer")
+            .withIssuer("your-issuer")  // ENV
             .withClaim("userId", userId)
-            .withExpiresAt(Date(System.currentTimeMillis() + 10 * 60 * 1000))
+            .withExpiresAt(Date(System.currentTimeMillis() + DAY))
             .sign(algorithm)
     }
 
@@ -22,7 +24,7 @@ object JwtConfig {
         return JWT.create()
             .withIssuer("your-issuer")
             .withClaim("userId", userId)
-            .withExpiresAt(Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000))
+            .withExpiresAt(Date(System.currentTimeMillis() + WEEK))
             .sign(algorithm)
     }
 }
