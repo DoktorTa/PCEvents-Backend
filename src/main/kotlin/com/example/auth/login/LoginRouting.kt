646dcb929a2a4387e5cd676dc1e2622d03977227
Aleksandr Balashov <sashabalashov2019@gmail.com>
2024-09-30 11:44:06 +0500

    3

--- src/main/kotlin/com/example/auth/login/LoginRouting.kt
+++ src/main/kotlin/com/example/auth/login/LoginRouting.kt
@@ -7,7 +7,7 @@ import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.utils.io.*
+
 
 fun Application.configureLoginRouting() {
     routing {
